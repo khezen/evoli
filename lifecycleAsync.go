8e12a789caa24e37cf632307b143871867a42b9b
khezen <guillaume@peaks.com>
2017-10-01 14:11:58 +0200

    concurency

--- lifecycleAsync.go
+++ lifecycleAsync.go
@@ -3,6 +3,8 @@ package darwin
 import (
 	"fmt"
 	"math/rand"
+
+	"github.com/khezen/check"
 )
 
 // lifecycle is a genetic algorithm implementation
@@ -15,6 +17,25 @@ func NewAsync(s Selecter, c Crosser, m Mutater, e Evaluater) Lifecycle {
 	return &lifecycleAsync{lifecycle{s, c, m, e}}
 }
 
+// Generation takes a population and produce a the new generation of this population
+func (l lifecycleAsync) Generation(pop Population, survivorSizeForSelection int, mutationProbability float32) (Population, error) {
+	err := check.NotNil(pop)
+	if err != nil {
+		return pop, err
+	}
+	newPop := l.evaluation(pop)
+	newPop, err = l.Selecter.Select(newPop, survivorSizeForSelection)
+	if err != nil {
+		return pop, err
+	}
+	newPop = l.crossovers(newPop)
+	newPop, err = l.mutations(newPop, mutationProbability)
+	if err != nil {
+		return pop, err
+	}
+	return newPop, nil
+}
+
 func (l lifecycleAsync) evaluation(pop Population) Population {
 	length := pop.Len()
 	dones := make([]chan bool, 0, length)
@@ -22,11 +43,12 @@ func (l lifecycleAsync) evaluation(pop Population) Population {
 		dones = append(dones, make(chan bool))
 	}
 	for i := 0; i < length; i++ {
-		go func(done chan bool) {
-			individual, _ := pop.Get(i)
+		go func(done chan bool, index int) {
+			individual, err := pop.Get(index)
+			fmt.Println(index, length, err)
 			individual.SetFitness(l.Evaluater.Evaluate(individual))
 			done <- true
-		}(dones[i])
+		}(dones[i], i)
 	}
 	for _, done := range dones {
 		<-done
@@ -64,14 +86,14 @@ func (l lifecycleAsync) mutations(pop Population, mutationProbability float32) (
 		dones = append(dones, make(chan bool))
 	}
 	for i := 0; i < length; i++ {
-		go func(done chan bool) {
+		go func(done chan bool, index int) {
 			if rand.Float32() <= mutationProbability {
-				indiv, _ := pop.Get(i)
+				indiv, _ := pop.Get(index)
 				mutant := l.Mutater.Mutate(indiv)
-				pop.Replace(i, mutant)
+				pop.Replace(index, mutant)
 			}
 			done <- true
-		}(dones[i])
+		}(dones[i], i)
 	}
 	for _, done := range dones {
 		<-done
