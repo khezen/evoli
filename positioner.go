f0e90883df3a51fd1d61ad40f26f62f1ced724e9
khezen <simonneaug@gmail.com>
2018-03-10 22:22:02 +0100

    swarm

--- positioner.go
+++ positioner.go
@@ -3,7 +3,8 @@ package evoli
 // Positioner produces a new individual from
 // (indiv) its previous version,
 // (pBest) its best version since it exists,
-// (gBest) and the best of the poopulation or local neighborhood
+// (gBest) the best of the poopulation or local neighborhood,
+// (c1,c2) and the learning coefficients. typical values are c1=c2=2
 type Positioner interface {
-	Position(indiv, pBest, gBest Individual, learningCoef1, learningCoef2 float64) (Individual, error)
+	Position(indiv, pBest, gBest Individual, c1, c2 float64) (Individual, error)
 }
