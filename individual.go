3d81a9366bb76feb8debf8e28ff3d745d7a3a5d8
khezen <simonneaug@gmail.com>
2018-03-10 18:59:28 +0100

    add pBest concept

--- individual.go
+++ individual.go
@@ -7,25 +7,33 @@ type Individual interface {
 	Fitness() float64
 	SetFitness(float64)
 	Equal(Individual) bool
+	Best() Individual
+	SetBest(Individual)
 }
 
 // individual https://en.wikipedia.org/wiki/individual
-type individual float64
+type individual struct {
+	fitness float64
+	best    Individual
+}
 
 // NewIndividual is the constructor for individuals
-func NewIndividual(Fitness float64) Individual {
-	indiv := individual(Fitness)
+func NewIndividual(fitness float64) Individual {
+	indiv := individual{
+		fitness,
+		nil,
+	}
 	return &indiv
 }
 
 // Fitness returns the strength of a individual regarding to its environement. Higher is stronger.
 func (indiv *individual) Fitness() float64 {
-	return float64(*indiv)
+	return indiv.fitness
 }
 
 // SetFitness set the strength of a individual regarding to its environement. Higher is stronger.
-func (indiv *individual) SetFitness(Fitness float64) {
-	*indiv = individual(Fitness)
+func (indiv *individual) SetFitness(fitness float64) {
+	indiv.fitness = fitness
 }
 
 // Equal return true if indiv is equal to toBeCompared
@@ -33,31 +41,56 @@ func (indiv *individual) Equal(toBeCompared Individual) bool {
 	return indiv == toBeCompared
 }
 
+func (indiv *individual) Best() Individual {
+	return indiv.best
+}
+
+func (indiv *individual) SetBest(best Individual) {
+	indiv.best = best
+}
+
 // individual https://en.wikipedia.org/wiki/individual
 type individualTS struct {
 	individual
-	mut sync.RWMutex
+	fitMut  sync.RWMutex
+	bestMut sync.RWMutex
 }
 
 // NewIndividualTS is the constructor for threadsafe individuals
-func NewIndividualTS(Fitness float64) Individual {
-	indiv := individual(Fitness)
+func NewIndividualTS(fitness float64) Individual {
+	indiv := individual{
+		fitness,
+		nil,
+	}
 	return &individualTS{
 		indiv,
+		sync.RWMutex{},
 		sync.RWMutex{},
 	}
 }
 
 // Fitness returns the strength of a individual regarding to its environement. Higher is stronger.
 func (indiv *individualTS) Fitness() float64 {
-	indiv.mut.RLock()
-	defer indiv.mut.RUnlock()
+	indiv.fitMut.RLock()
+	defer indiv.fitMut.RUnlock()
 	return indiv.individual.Fitness()
 }
 
 // SetFitness set the strength of a individual regarding to its environement. Higher is stronger.
 func (indiv *individualTS) SetFitness(Fitness float64) {
-	indiv.mut.Lock()
-	defer indiv.mut.Unlock()
+	indiv.fitMut.Lock()
+	defer indiv.fitMut.Unlock()
 	indiv.individual.SetFitness(Fitness)
+}
+
+func (indiv *individualTS) Best() Individual {
+	indiv.bestMut.RLock()
+	defer indiv.bestMut.RUnlock()
+	return indiv.individual.Best()
+}
+
+func (indiv *individualTS) SetBest(best Individual) {
+	indiv.bestMut.Lock()
+	defer indiv.bestMut.Unlock()
+	indiv.individual.SetBest(best)
 }
--- individual_test.go
+++ individual_test.go
@@ -22,6 +22,31 @@ func TestNewIndividual(t *testing.T) {
 	}
 }
 
+func TestBest(t *testing.T) {
+	i0, i10 := NewIndividual(0), NewIndividual(10)
+	i0TS, i10TS := NewIndividualTS(0), NewIndividualTS(10)
+	cases := []struct {
+		indiv        Individual
+		in, expected Individual
+	}{
+		{i0, i0, i0},
+		{i0, i10, i10},
+		{i10, i10, i10},
+		{i10, i0, i0},
+		{i0TS, i0TS, i0TS},
+		{i0TS, i10TS, i10TS},
+		{i10TS, i10TS, i10TS},
+		{i10TS, i0TS, i0TS},
+	}
+	for _, c := range cases {
+		c.indiv.SetBest(c.in)
+		got := c.indiv.Best()
+		if got != c.expected {
+			t.Errorf("expected %v got %v", c.expected, got)
+		}
+	}
+}
+
 func TestFitness(t *testing.T) {
 	cases := []struct {
 		indiv        Individual
