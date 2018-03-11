651c7f9399611c72de7230d64d8470713f13206a
khezen <simonneaug@gmail.com>
2018-03-11 19:08:24 +0100

    edit doc

--- positioner.go
+++ positioner.go
@@ -1,10 +1,10 @@
 package evoli
 
 // Positioner produces a new individual from
-// (indiv) its previous version,
-// (pBest) its best version since it exists,
-// (gBest) the best of the poopulation or local neighborhood,
-// (c1,c2) and the learning coefficients. typical values are c1=c2=2
+// its previous version(indiv),
+// its best version since it exists(pBest),
+// the best of the poopulation(gBest)  or local neighborhood,
+// and the learning coefficients(c1,c2). typical values are c1=c2=2
 type Positioner interface {
 	Position(indiv, pBest, gBest Individual, c1, c2 float64) (Individual, error)
 }
