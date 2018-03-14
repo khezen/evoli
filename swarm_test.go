057fc7457b8915afd6ee3d3230ecca0a4d85ae28
khezen <simonneaug@gmail.com>
2018-03-14 01:13:14 +0100

    test swarm

--- swarm_test.go
+++ swarm_test.go
@@ -12,8 +12,39 @@ func TestNewSwarm(t *testing.T) {
 		{positionerMock{}, 2, 2, evaluaterMock{}},
 	}
 	for _, c := range errorCases {
-		_ = NewSwarm(NewPopulation(1), c.p, c.c1, c.c2, c.e)
-		_ = NewSwarmSync(NewPopulation(1), c.p, c.c1, c.c2, c.e)
+		i1 := NewIndividual(1)
+		i2 := NewIndividual(2)
+		popInit := NewPopulation(2)
+		newPop := NewPopulation(1)
+		popInit.Add(i1, i2)
+		sw := NewSwarm(popInit, c.p, c.c1, c.c2, c.e)
+		pop := sw.Population()
+		if pop != popInit {
+			t.Errorf("expected %v got %v", popInit, pop)
+		}
+		alpha := sw.Alpha()
+		if alpha != i2 {
+			t.Errorf("expected %v got %v", i2, alpha)
+		}
+		sw.SetPopulation(newPop)
+		pop = sw.Population()
+		if pop != newPop {
+			t.Errorf("expected %v got %v", newPop, pop)
+		}
+		sw = NewSwarmSync(popInit, c.p, c.c1, c.c2, c.e)
+		pop = sw.Population()
+		if pop != popInit {
+			t.Errorf("expected %v got %v", popInit, pop)
+		}
+		alpha = sw.Alpha()
+		if alpha != i2 {
+			t.Errorf("expected %v got %v", i2, alpha)
+		}
+		sw.SetPopulation(newPop)
+		pop = sw.Population()
+		if pop != newPop {
+			t.Errorf("expected %v got %v", newPop, pop)
+		}
 	}
 }
 
