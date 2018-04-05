93a93c244d3355fd7263b1c7d6a7b54b512308e8
Guillaume Simonneau <2980507+khezen@users.noreply.github.com>
2018-04-05 11:12:21 +0200

    Update evaluater.go

--- evaluater.go
+++ evaluater.go
@@ -1,6 +1,6 @@
 package evoli
 
-// Evaluater calculate individual Fitness
+// Evaluater calculates individual Fitness
 type Evaluater interface {
 	Evaluate(Individual) (Fitness float64, err error)
 }
